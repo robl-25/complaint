5b6f5a263ded80c2c877b3724087818adc7ce5ac
robl-25 <marianerobl@protonmail.com>
2019-07-07 23:00:30 -0300

    create more search tests

--- spec/controllers/api/v1/company_controller_spec.rb
+++ spec/controllers/api/v1/company_controller_spec.rb
@@ -15,5 +15,41 @@ RSpec.describe Api::V1::CompanyController, type: :controller do
         expect(results).to eq(expected_results)
       end
     end
+
+    context 'search with uppercase' do
+      it 'should return one result' do
+        get :search, params: { term: 'TeS' }
+
+        expect(response.status).to eq(200)
+
+        expected_results = [{ "text" => company.name, "id" => company.id }]
+        results = JSON.parse(response.body)['results']
+        expect(results).to eq(expected_results)
+      end
+    end
+
+    context "search company that doesn't exists" do
+      it 'should return one result' do
+        get :search, params: { term: '123' }
+
+        expect(response.status).to eq(200)
+
+        expected_results = []
+        results = JSON.parse(response.body)['results']
+        expect(results).to eq(expected_results)
+      end
+    end
+  end
+
+  describe 'when does not exists companies' do
+    it 'should return empty results' do
+      get :search, params: { term: 'Tes' }
+
+      expect(response.status).to eq(200)
+
+      expected_results = []
+      results = JSON.parse(response.body)['results']
+      expect(results).to eq(expected_results)
+    end
   end
 end
--- spec/controllers/api/v1/locale_controller_spec.rb
+++ spec/controllers/api/v1/locale_controller_spec.rb
@@ -17,5 +17,43 @@ RSpec.describe Api::V1::LocaleController, type: :controller do
         expect(results).to eq(expected_results)
       end
     end
+
+    context 'search with uppercase' do
+      it 'should return one result' do
+        get :search, params: { term: 'TeS' }
+
+        expect(response.status).to eq(200)
+
+        expected_results = [
+          { "text" => "#{locale.city} - #{locale.state}", "id" => locale.id }
+        ]
+        results = JSON.parse(response.body)['results']
+        expect(results).to eq(expected_results)
+      end
+    end
+
+    context "search city that doesn't exists" do
+      it 'should return one result' do
+        get :search, params: { term: '123' }
+
+        expect(response.status).to eq(200)
+
+        expected_results = []
+        results = JSON.parse(response.body)['results']
+        expect(results).to eq(expected_results)
+      end
+    end
+  end
+
+  describe 'when does not exists locale' do
+    it 'should return empty results' do
+      get :search, params: { term: 'Tes' }
+
+      expect(response.status).to eq(200)
+
+      expected_results = []
+      results = JSON.parse(response.body)['results']
+      expect(results).to eq(expected_results)
+    end
   end
 end
